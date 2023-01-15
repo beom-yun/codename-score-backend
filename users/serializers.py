1fdc99b524b4e08db5325d01e63659184001dfcb
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-01-15 19:27:39 +0900

    change-password, login, logout

--- users/serializers.py
+++ users/serializers.py
@@ -6,3 +6,19 @@ class BowlerSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ("name",)
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+            "password",
+            "is_superuser",
+            "is_staff",
+            "is_active",
+            "date_joined",
+            "first_name",
+            "last_name",
+            "groups",
+            "user_permissions",
+        )
