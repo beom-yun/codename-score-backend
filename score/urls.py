d2ea81dfea71e920efd86a2892672157aefd7934
beom-yun <scds0326@naver.com>
2023-01-16 00:35:24 +0900

    Regular Game Dates views

--- score/urls.py
+++ score/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.RegularGameDates.as_view()),
     path("<int:pk>/", views.RegularGameScores.as_view()),
 ]
--- score/views.py
+++ score/views.py
@@ -1,7 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import RegularGameScore, RegularGameDate
-from .serializers import RegularGameSerializer
+from .serializers import RegularGameSerializer, RegularGameDateSerializer
+
+
+class RegularGameDates(APIView):
+    def get(self, request):
+        dates = RegularGameDate.objects.all()
+        serializer = RegularGameDateSerializer(dates, many=True)
+        return Response(serializer.data)
 
 
 class RegularGameScores(APIView):
