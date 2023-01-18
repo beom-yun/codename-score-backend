17f66e194ba6c5875d9ca42cc97413ad74cd3c2d
beom-yun <scds0326@naver.com>
2023-01-18 15:45:00 +0900

    RegularGameDateDetail

--- score/urls.py
+++ score/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.RegularGameDates.as_view()),
     path("<int:pk>/", views.RegularGameScores.as_view()),
+    path("dates/", views.RegularGameDates.as_view()),
+    path("dates/<int:pk>/", views.RegularGameDateDetail.as_view()),
 ]
--- score/views.py
+++ score/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import ParseError
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.exceptions import ParseError, NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import RegularGameScore, RegularGameDate
 from .serializers import RegularGameSerializer, RegularGameDateSerializer
@@ -30,8 +30,46 @@ class RegularGameDates(APIView):
         else:
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-    def delete(self, request):
-        pass
+
+class RegularGameDateDetail(APIView):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            return RegularGameDate.objects.get(pk=pk)
+        except RegularGameDate.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        regular_game_date = self.get_object(pk)
+        serializer = RegularGameDateSerializer(regular_game_date)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        regular_game_date = self.get_object(pk)
+        serializer = RegularGameDateSerializer(
+            regular_game_date, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            round_of_game = request.data.get("round_of_game")
+            date = request.data.get("date")
+            if (
+                round_of_game
+                and RegularGameDate.objects.filter(round_of_game=round_of_game).exists()
+            ):
+                raise ParseError("이미 존재하는 회차입니다.")
+            if date and RegularGameDate.objects.filter(date=date).exists():
+                raise ParseError("이미 정기전이 진행된 날짜입니다.")
+            updated_regular_game_date = serializer.save()
+            return Response(RegularGameDateSerializer(updated_regular_game_date).data)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        regular_game_date = self.get_object(pk)
+        regular_game_date.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
 
 
 class RegularGameScores(APIView):
