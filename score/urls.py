79dd766ef156ecdab59076222067cd9076983e46
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-08-29 10:12:52 +0900

    Reset

--- score/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("<int:pk>/", views.RegularGameScores.as_view()),
-    path("dates/", views.RegularGameDates.as_view()),
-    path("dates/<int:pk>/", views.RegularGameDateDetail.as_view()),
-]
--- users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    first_name = models.CharField(max_length=150, editable=False)
-    last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150, default="")
-    phone_number = models.CharField(max_length=11, null=True)
